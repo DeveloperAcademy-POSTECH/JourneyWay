899cefd8388df347348eddffda994b13e0e58d3a
Ayden <kaitaro77@naver.com>
2022-04-06 10:40:52 +0900

    project init

---
+++ WheelchairRun/WheelchairRun/WheelchairRunApp.swift
@@ -0,0 +1,20 @@
+//
+//  WheelchairRunApp.swift
+//  WheelchairRun
+//
+//  Created by Ayden on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct WheelchairRunApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
