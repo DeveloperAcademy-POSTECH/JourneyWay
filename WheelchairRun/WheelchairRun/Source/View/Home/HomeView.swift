f66f8d64373273ce01fba62633e5a1877a29f9d7
pis3120 <40324511+pis3120@users.noreply.github.com>
2022-04-07 10:31:46 +0900

    Update_Home view UI

---
+++ WheelchairRun/WheelchairRun/Source/View/Home/HomeView.swift
@@ -0,0 +1,132 @@
+//
+//  HomeView.swift
+//  WheelchairRun
+//
+//  Created by  Isac Park on 2022/04/06.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Spacer()
+            HStack(alignment: .top, spacing: 10){
+                Spacer()
+                VStack(alignment: .leading, spacing: 5){
+                    HStack{
+                        Text("안녕하세요,")
+                            .font(.title3)
+                        Text("요셉")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                    Text("보이스멘토와 함께")
+                        .font(.title3)
+                    Text("휠체어를 밀어보세요!")
+                        .font(.title3)
+                    
+                }
+                Spacer()
+                Button(action: {print("Profile_Button")}){
+                    Image("Profile_img")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+                Spacer()
+            }
+            Spacer()
+            Button(action: {print("pushNgo_Button")}){
+                Image("pushNgo_img")
+                    .resizable()
+                    .frame(width: 220, height: 220)
+            }
+            Spacer()
+            
+            
+            Button {
+                print("Today_Record_Button")
+            } label: {
+                VStack(alignment: .leading, spacing: 25){
+                    HStack(spacing: 0.0){
+                        
+                        Image("Today_Record_img")
+                            .resizable()
+                            .frame(width: 120, height: 40)
+                        
+                            .padding(.horizontal, 30)
+                        
+                    }
+                    .padding(.vertical, 10)
+                    HStack{
+                        Spacer()
+                        Image("Clock_img")
+                            .resizable()
+                            .frame(width: 32.0, height: 32.0)
+                        Spacer()
+                        Image("Kcal_img")
+                            .resizable()
+                            .frame(width: 38.0, height: 38.0)
+                        Spacer()
+                        Image("Push_img")
+                            .resizable()
+                            .frame(width: 32.0, height: 32.0)
+                        Spacer()
+                    }
+                    .padding(.horizontal, 3)
+                    .padding(.vertical, -20)
+                    
+                    HStack {
+                        Spacer()
+                        VStack(alignment: .center, spacing: 5){
+                            Text("00:00")
+                                .foregroundColor(Color.black)
+                                .font(.title3)
+                            Text("Time")
+                                .font(.footnote)
+                                .foregroundColor(Color.blue)
+                            
+                        }
+                        Spacer()
+                        VStack(alignment: .center, spacing: 5){
+                            Text("132")
+                                .foregroundColor(Color.black)
+                                .font(.title3)
+                            Text("Kcal")
+                                .font(.footnote)
+                                .foregroundColor(Color.blue)
+                            
+                        }
+                        Spacer()
+                        VStack(alignment: .center, spacing: 5){
+                            Text("1422")
+                                .foregroundColor(Color.black)
+                                .font(.title3)
+                            Text("Push")
+                                .font(.footnote)
+                                .foregroundColor(Color.blue)
+                        }
+                        .padding(.horizontal, 4)
+                        Spacer()
+                    }
+                    
+                }
+            }
+            
+            Spacer()
+            Button(action: {print("with_voice_mentor_Button")}){
+                Image("with_voice_mentor_img")
+                    .resizable()
+                    .frame(width: 230, height: 50)
+            }
+        }
+        .padding(.vertical, 20)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
