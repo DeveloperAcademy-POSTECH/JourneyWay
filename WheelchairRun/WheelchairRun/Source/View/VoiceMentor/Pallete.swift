486f44272fd3ada14e7cca7547dd170e6fab6091
wody-d <maxwodyd98@gmail.com>
2022-04-08 12:07:48 +0900

    feat: 기본적인 Color 추가

---
+++ WheelchairRun/WheelchairRun/Source/View/VoiceMentor/Pallete.swift
@@ -0,0 +1,21 @@
+//
+//  Pallete.swift
+//  WheelchairRun
+//
+//  Created by Woody on 2022/04/08.
+//
+
+import SwiftUI
+
+/// 앱에 사용되는 모든 색상
+struct Pallete {
+    static let background: Color        = .white
+    static let cardPointColor1: Color   = .init(hex: "#7900FF")
+    static let cardPointColor2: Color   = .init(hex: "#F89D55")
+    static let cardPointColor3: Color   = .init(hex: "#E45151")
+    static let cardPointColor4: Color   = .init(hex: "#18D87B")
+    static let purple: Color            = .init(hex: "#7900FF")
+    static let blue: Color              = .init(hex: "#548CFF")
+    static let mint: Color              = .init(hex: "#93FFD8")
+    static let lightMint: Color         = .init(hex: "#CFFFDC")
+}
