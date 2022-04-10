ea699fd106e34ca2280bba73a16b9b83cd0b7650
wody-d <maxwodyd98@gmail.com>
2022-04-10 11:07:02 +0900

    feat: VoiceMentorList 생성

---
+++ WheelchairRun/WheelchairRun/Source/View/VoiceMentor/RecentVoiceMentorList.swift
@@ -0,0 +1,38 @@
+//
+//  RecentVoiceMentorList.swift
+//  WheelchairRun
+//
+//  Created by Woody on 2022/04/10.
+//
+
+import SwiftUI
+
+struct RecentVoiceMentorList: View {
+    
+    // TODO: 폰트 수정
+    var body: some View {
+        VStack {
+            Text("Recent")
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.system(size: 25))
+                .padding(.leading, 24)
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 12) {
+                    Color.clear.padding(2)
+                    ForEach(1..<100) { _ in
+                        VoiceMentorCardView()
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: 130)
+        }
+        
+    }
+}
+
+struct RecentVoiceMentorList_Previews: PreviewProvider {
+    static var previews: some View {
+        RecentVoiceMentorList()
+    }
+}
