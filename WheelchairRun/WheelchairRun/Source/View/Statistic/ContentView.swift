2487d1b50303eabfb504ff3adbde1d1f1386a571
JackJeon <letschutzpah430@gmail.com>
2022-04-11 11:19:10 +0900

    20220411_Statistic_Jack

--- WheelchairRun/WheelchairRun/Source/View/Statistic/ContentView.swift
+++ WheelchairRun/WheelchairRun/Source/View/Statistic/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+var contents = ["4월 2일", "4월 1일", "3월 31일", "3월 30일", "3월 29일", "3월 28일", "3월 27일"]
+
 struct ContentView: View {
     var body: some View {
             HomeView()
@@ -74,23 +76,68 @@ struct StatisticView2: View {
     var body: some View {
         
         VStack(alignment: .leading) {
-        Text("푸쉬 다이어리")
-            .font(.custom("Apple SD Gothic Neo Light", size: 18)).bold()
-            .padding(EdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 0))
+            Text("푸쉬 다이어리")
+                .font(.custom("Apple SD Gothic Neo Light", size: 18)).bold()
+                .padding(EdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 0))
             
-        Text("매일 매일 운동 기록을 채워보세요!")
-            .font(.custom("Apple SD Gothic Neo Light", size: 12))
-            .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 0))
+            Text("매일 매일 운동 기록을 채워보세요!")
+                .font(.custom("Apple SD Gothic Neo Light", size: 12))
+                .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 0))
             
-        List{
-            Text("3월 30일")
-            Text("4월 1일")
-        }
-
+            ScrollView {
+                LazyVStack(pinnedViews: [.sectionHeaders]) {
+                    Section(header: Header()) {
+                        ForEach(contents, id: \.self) { name in
+                            ListItem(title: name)
+                                .padding(.horizontal, 12)
+                                .padding(.vertical, 4)
+                        }
+                    }
+                }
+            }.clipped()
         }.navigationTitle("통계")
             .navigationBarTitleDisplayMode(.inline)
     }
 }
+
+struct ListItem: View {
+    let title: String
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            HStack {
+                Text("거리").padding()
+                Text("푸쉬").padding()
+                Text("칼로리").padding()
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .frame(height: 100)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundColor(.blue)
+                .opacity(0.2)
+            Text(title)
+                .foregroundColor(.black)
+                .fontWeight(.bold)
+        }
+    }
+}
+
+struct Header: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("2022년")
+                .fontWeight(.bold)
+            Spacer()
+            Divider()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .frame(height: 56)
+        .background(Rectangle().foregroundColor(.white))
+    }
+}
+
 
 
 struct SwiftUIView_Previews: PreviewProvider {
