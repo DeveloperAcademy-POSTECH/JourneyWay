a0c989bc2cf26bd8e04f4002b8b0a414186746fd
Ji Woong Kang <ipkjw2@gmail.com>
2022-04-14 17:43:24 +0900

    메달 추가 및 BarGraph, DiaryData 수정

--- WheelchairRun/WheelchairRun/Source/View/Statistic/PushDiaryView.swift
+++ WheelchairRun/WheelchairRun/Source/View/Statistic/PushDiaryView.swift
@@ -1,3 +1,65 @@
+////
+////  PushDiaryView.swift
+////  WheelchairRun
+////
+////  Created by JiwKang on 2022/04/12.
+////
+//
+//import SwiftUI
+//
+//struct PushDiaryView: View {
+//    let diarys: [DiaryData] = DiaryData.sampleData
+//    var currentYear: Int = 2022
+//
+//    var body: some View {
+//
+//        VStack(alignment: .leading) {
+//            Text("푸쉬 다이어리")
+//                .font(.custom("Apple SD Gothic Neo Light", size: 22)).bold()
+//                .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0))
+//
+//            Text("매일 매일 운동 기록을 채워보세요!")
+//                .font(.custom("Apple SD Gothic Neo Light", size: 15))
+//                .padding(EdgeInsets(top: 0, leading: 11, bottom: 0, trailing: 0))
+//
+//            ScrollView {
+//                LazyVStack(pinnedViews: [.sectionHeaders]) {
+//                    Section(header: Header(currentYear: String(currentYear))) {
+//                        ForEach(0..<diarys.count, id: \.self) { index in
+//                            DiaryList(diary: diarys[index])
+//                            Spacer()
+//                        }
+//                    }
+//                }
+//            }.clipped()
+//        }.navigationTitle("통계")
+//            .navigationBarTitleDisplayMode(.inline)
+//            .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+//    }
+//}
+//
+//struct Header: View {
+//    var currentYear: String
+//    var body: some View {
+//        VStack {
+//            Spacer()
+//            Text("\(currentYear)년")
+//                .fontWeight(.bold)
+//            Spacer()
+//            Divider()
+//        }
+//        .frame(minWidth: 0, maxWidth: .infinity)
+//        .frame(height: 40)
+//        .background(Rectangle().foregroundColor(.white).opacity(0.7))
+//    }
+//}
+//
+//struct PushDiaryView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PushDiaryView()
+//    }
+//}
+
 //
 //  PushDiaryView.swift
 //  WheelchairRun
@@ -9,7 +71,6 @@ import SwiftUI
 
 struct PushDiaryView: View {
     let diarys: [DiaryData] = DiaryData.sampleData
-    var currentYear: Int = 2022
     
     var body: some View {
         
@@ -22,35 +83,41 @@ struct PushDiaryView: View {
                 .font(.custom("Apple SD Gothic Neo Light", size: 15))
                 .padding(EdgeInsets(top: 0, leading: 11, bottom: 0, trailing: 0))
             
+            
             ScrollView {
                 LazyVStack(pinnedViews: [.sectionHeaders]) {
-                    Section(header: Header(currentYear: String(currentYear))) {
-                        ForEach(0..<diarys.count, id: \.self) { index in
-                            DiaryList(diary: diarys[index])
-                            Spacer()
+                    ForEach(0..<diarys.count, id: \.self) { index in
+                        Section(header: Header(currentYear: diarys[index].getYear())) {
+                            ForEach(0..<diarys[index].getValues().count, id: \.self) { i in
+                                DiaryList(diary: diarys[index].getValues()[i])
+                            }
                         }
                     }
                 }
-            }.clipped()
-        }.navigationTitle("통계")
-            .navigationBarTitleDisplayMode(.inline)
-            .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+            }
+        }
+        .navigationTitle("통계")
+        .navigationBarTitleDisplayMode(.inline)
+        .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
     }
 }
 
 struct Header: View {
-    var currentYear: String
+    var currentYear: Int
     var body: some View {
         VStack {
-            Spacer()
             Text("\(currentYear)년")
                 .fontWeight(.bold)
-            Spacer()
             Divider()
         }
         .frame(minWidth: 0, maxWidth: .infinity)
-        .frame(height: 40)
-        .background(Rectangle().foregroundColor(.white).opacity(0.7))
+        .background(
+            ZStack {
+                let color = Color.white
+                
+                LinearGradient(gradient: .init(colors:[color, color, color.opacity(0.9), color.opacity(0.7)]), startPoint: .top, endPoint: .bottom)
+            }
+        )
     }
 }
 
