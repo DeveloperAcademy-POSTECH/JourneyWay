0ce057ad57452afe1262199747d56f7fff8b3a00
wody-d <maxwodyd98@gmail.com>
2022-04-21 17:25:39 +0900

    refactor: 파일들 폴더 위치 변경

--- WheelchairRun/WheelchairRun/Source/Views/Home/ContentView.swift
+++ WheelchairRun/WheelchairRun/Source/Views/Home/ContentView.swift
@@ -23,15 +23,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-        // TODO: 나스닥한테 아래 코드 의미 물어보기 - Preview에 넣으면 빌드에서는 작동하지 않는 코드인데 필요한가?
+        // TODO: 나스닥한테 아래 코드 의미 물어보기
             .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
-
-// TODO: 나스닥한테 아래 코드 의미 물어보기
-//private let itemFormatter: DateFormatter = {
-//    let formatter = DateFormatter()
-//    formatter.dateStyle = .short
-//    formatter.timeStyle = .medium
-//    return formatter
-//}()
